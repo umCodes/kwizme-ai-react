e0387799293c5b57ae7b0dbf134a363ebe3c6ea5
umCodes <umohammed.official@gmail.com>
2025-03-18 07:32:50 +0300

    deployment commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/kwizme-ai-react"
+  plugins: [react(), tailwindcss()]
 })
